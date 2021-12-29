5c54e6557ed3ca829840ce08757a40dd1a71e211
LektoriumSymbol <74270066+LektoriumSymbol@users.noreply.github.com>
2021-12-29 21:56:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 function App() {
+  const par = document.createElement('p');
+  par.textContent = 'This is also visible!';
+  document.getElementById('root').append(par);
   return (
     <div>
         <h2>
           Let's get started!
         </h2>
+        <p>This is also visible!</p>
+        {/* <p>This is also visible!</p> */}
     </div>
   );
 }
