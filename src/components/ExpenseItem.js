baffddeebc505d6e3161bfb5721f22775c101697
LektoriumSymbol <74270066+LektoriumSymbol@users.noreply.github.com>
2021-12-30 23:33:24 +0200

    Add expenses props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import './ExpenseItem.css' 
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <p>March 28th 2021</p>
+      <p>{props.date.toISOString()}</p>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
