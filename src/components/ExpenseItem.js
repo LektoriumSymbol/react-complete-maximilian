ce05ef2706f8fa952ee4d46a70969c90f8ad9e2d
LektoriumSymbol <74270066+LektoriumSymbol@users.noreply.github.com>
2022-01-01 14:51:55 +0200

    Format expenses date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
-import './ExpenseItem.css' 
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <p>{props.date.toISOString()}</p>
+      <div>
+        <p>{month}</p>
+        <p>{day}</p>
+        <p>{year}</p>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
